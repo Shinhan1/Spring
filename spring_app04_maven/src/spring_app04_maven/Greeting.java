aa5f06a6741ea51d114fdec3f3ed295811e27bf1
Shinhan1 <40006670+Shinhan1@users.noreply.github.com>
2020-05-20 16:21:59 +0900

    string

---
+++ spring_app04_maven/src/spring_app04_maven/Greeting.java
@@ -0,0 +1,11 @@
+package spring_app04_maven;
+
+public class Greeting implements Message {
+	
+	@Override
+	public void sayHello(String msg) {
+		// TODO Auto-generated method stub
+		System.out.println(msg);
+	}
+	
+}

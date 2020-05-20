7b2d7de296e0d0d93682285671be16cb407f8402
Shinhan1 <40006670+Shinhan1@users.noreply.github.com>
2020-05-20 16:29:05 +0900

    spring

---
+++ spring_app02/src/spring_app02/TeaImple.java
@@ -0,0 +1,23 @@
+package spring_app02;
+
+public class TeaImple implements Beverage {
+
+	String product;
+	
+	public TeaImple() {
+		this.product = "Green Tea";
+	}
+	
+	public TeaImple(String product) {
+		this.product = product;
+	}
+	
+	
+	
+	@Override
+	public void drink(String name) {
+		System.out.println(name + "님이" + product + "을 홀짝홀짝 마셔요");
+
+	}
+
+}
